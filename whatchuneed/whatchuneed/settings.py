780ae28857a2ac28750c14b8a1050e574b333892
DuriDuri <durid@union.edu>
2014-03-24 17:54:59 -0400

    Update settings.py

--- whatchuneed/whatchuneed/settings.py
+++ whatchuneed/whatchuneed/settings.py
@@ -15,7 +15,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'duri.abdurahman@gmail.com'
-EMAIL_HOST_PASSWORD = 'Allaroundtheworld'
+EMAIL_HOST_PASSWORD = '*********'
 
 
 MANAGERS = ADMINS
