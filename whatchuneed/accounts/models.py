5308e460e49e9a7b2ef1c4ec5cad8931d813b0d6
Duri <durid@union.edu>
2014-03-27 16:16:47 -0400

    rm Userena

--- whatchuneed/accounts/models.py
+++ whatchuneed/accounts/models.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _  
 from userena.models import UserenaBaseProfile  
   
-class MyProfile(UserenaBaseProfile):  
-    user = models.OneToOneField(User,unique=True,  
-                        verbose_name=_('user'),related_name='my_profile')  
-    phone = models.IntegerField(max_length=10, unique=True)
-    email_address = models.EmailField(max_length=50)
+class MyProfile(UserenaBaseProfile):
+    user = models.OneToOneField(User,
+                                unique=True,
+                                verbose_name=_('user'),
+                                related_name='my_profile')
+    favourite_snack = models.CharField(_('favourite snack'),
+                                       max_length=5)
     
